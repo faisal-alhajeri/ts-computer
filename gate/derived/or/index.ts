f5eeb80d857813a29cbd8567a9e0e94a6b6f2671
faisal alhajeri <f.alhajeri@nearpay.io>
2024-02-16 17:17:18 +0300

    or refactor

--- gate/derived/or/index.ts
+++ gate/derived/or/index.ts
@@ -1,12 +1,18 @@
 import { Bit, Gate } from "../..";
+import { NandGate } from "../../base/nand";
+import { AndGate } from "../and";
+import { NotGate } from "../not";
 
 type Inputs = [Bit, Bit];
 type Outputs = [Bit];
 
 export class OrGate extends Gate<Inputs, Outputs> {
   override eval(inputs: Inputs): Outputs {
-    if (inputs[0] === 0 && inputs[1] === 0) return [0];
+    const [x, y] = inputs;
+    const nandGate = new NandGate();
+    const notGate1 = new NotGate();
+    const notGate2 = new NotGate();
 
-    return [1];
+    return nandGate.eval([...notGate1.eval([x]), ...notGate2.eval([y])]);
   }
 }
