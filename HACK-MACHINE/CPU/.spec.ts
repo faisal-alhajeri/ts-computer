8a12031f1dd9e2dd0eb2fdf21492675005356fa8
faisal <faisal.alhajri1420@gmail.com>
2024-03-30 17:21:13 +0300

    jump test

--- HACK-MACHINE/CPU/.spec.ts
+++ HACK-MACHINE/CPU/.spec.ts
@@ -20,7 +20,15 @@ type CPUPartialResult = Partial<{
 }>;
 
 class Util {
-  private static clock: Bit = 0;
+  private static _clock: Bit = 0;
+
+  static get clock() {
+    const res = this._clock;
+    this._clock = (this._clock ^ 1) as Bit;
+
+    return res;
+  }
+
   static randomBit(): Bit {
     return Math.random() > 0.5 ? 1 : 0;
   }
@@ -50,8 +58,6 @@ class Util {
       reset,
       this.clock,
     ]);
-
-    this.clock = (this.clock ^ 1) as Bit;
 
     if (expected.outM !== undefined) expect(outM).toEqual(expected.outM);
 
@@ -194,5 +200,298 @@ describe("CPU", () => {
   });
 
   // TODO: complete this test
-  test("(c) operations jumps", async () => {});
+  test("(c) operations jumps", async () => {
+    // we will change address to 0 every time then do a test
+    const cpu = new CPU();
+    const JUMP_PC = Util.formStrToBitArray("0000000000001000");
+    const NO_JUMP_PC = Util.formStrToBitArray("0000000000000010");
+    const RESET_PC = Util.formStrToBitArray("0000000000000000");
+    const M = Util.formStrToBitArray("1010000000000000");
+    const D = Util.formStrToBitArray("0000000000001010");
+
+    const D_INSTRUCTION_PRFIX = "1110001100000";
+    const M_INSTRUCTION_PRFIX = "1111110000000";
+    const D_AND_M_INSTRUCTION_PRFIX = "1111000000000";
+
+    // D = M (and reset PC)
+    await cpu.eval([
+      D,
+      Util.formStrToBitArray("1111110000010000"),
+      1,
+      Util.clock,
+    ]);
+
+    const experements: CPUTest[] = [
+      // -=-=-=-=-=-=-=-=-=- null -=-=-=-=-=-=-=-=-=-=
+      // D; null -> no jump
+      {
+        inputs: {
+          inM: M,
+          instruction: Util.formStrToBitArray(`${D_INSTRUCTION_PRFIX}000`),
+        },
+        expected: {
+          pc: NO_JUMP_PC,
+        },
+      },
+      // M; null -> no jump
+      {
+        inputs: {
+          inM: M,
+          instruction: Util.formStrToBitArray(`${M_INSTRUCTION_PRFIX}000`),
+        },
+        expected: {
+          pc: NO_JUMP_PC,
+        },
+      },
+      // D&M; null -> no jump
+      {
+        inputs: {
+          inM: M,
+          instruction: Util.formStrToBitArray(
+            `${D_AND_M_INSTRUCTION_PRFIX}000`
+          ),
+        },
+        expected: {
+          pc: NO_JUMP_PC,
+        },
+      },
+      // -=-=-=-=-=-=-=-=-=- JGT -=-=-=-=-=-=-=-=-=-=
+      // D; JGT -> jump
+      {
+        inputs: {
+          inM: M,
+          instruction: Util.formStrToBitArray(`${D_INSTRUCTION_PRFIX}001`),
+        },
+        expected: {
+          pc: JUMP_PC,
+        },
+      },
+      // M; JGT -> no jump
+      {
+        inputs: {
+          inM: M,
+          instruction: Util.formStrToBitArray(`${M_INSTRUCTION_PRFIX}001`),
+        },
+        expected: {
+          pc: NO_JUMP_PC,
+        },
+      },
+      // D&M; JGT -> no jump
+      {
+        inputs: {
+          inM: M,
+          instruction: Util.formStrToBitArray(
+            `${D_AND_M_INSTRUCTION_PRFIX}001`
+          ),
+        },
+        expected: {
+          pc: NO_JUMP_PC,
+        },
+      },
+      // -=-=-=-=-=-=-=-=-=- JEQ -=-=-=-=-=-=-=-=-=-=
+      // D; JEQ -> no jump
+      {
+        inputs: {
+          inM: M,
+          instruction: Util.formStrToBitArray(`${D_INSTRUCTION_PRFIX}010`),
+        },
+        expected: {
+          pc: NO_JUMP_PC,
+        },
+      },
+      // M; JEQ -> no jump
+      {
+        inputs: {
+          inM: M,
+          instruction: Util.formStrToBitArray(`${M_INSTRUCTION_PRFIX}010`),
+        },
+        expected: {
+          pc: NO_JUMP_PC,
+        },
+      },
+      // D&M; JEQ ->  jump
+      {
+        inputs: {
+          inM: M,
+          instruction: Util.formStrToBitArray(
+            `${D_AND_M_INSTRUCTION_PRFIX}010`
+          ),
+        },
+        expected: {
+          pc: JUMP_PC,
+        },
+      },
+      // -=-=-=-=-=-=-=-=-=- JGE -=-=-=-=-=-=-=-=-=-=
+      // D; JGE ->  jump
+      {
+        inputs: {
+          inM: M,
+          instruction: Util.formStrToBitArray(`${D_INSTRUCTION_PRFIX}011`),
+        },
+        expected: {
+          pc: JUMP_PC,
+        },
+      },
+      // M; JGE -> no jump
+      {
+        inputs: {
+          inM: M,
+          instruction: Util.formStrToBitArray(`${M_INSTRUCTION_PRFIX}011`),
+        },
+        expected: {
+          pc: NO_JUMP_PC,
+        },
+      },
+      // D&M; JGE ->  jump
+      {
+        inputs: {
+          inM: M,
+          instruction: Util.formStrToBitArray(
+            `${D_AND_M_INSTRUCTION_PRFIX}011`
+          ),
+        },
+        expected: {
+          pc: JUMP_PC,
+        },
+      },
+      // -=-=-=-=-=-=-=-=-=- JLT -=-=-=-=-=-=-=-=-=-=
+      // D; JLT -> no jump
+      {
+        inputs: {
+          inM: M,
+          instruction: Util.formStrToBitArray(`${D_INSTRUCTION_PRFIX}100`),
+        },
+        expected: {
+          pc: NO_JUMP_PC,
+        },
+      },
+      // M; JLT ->  jump
+      {
+        inputs: {
+          inM: M,
+          instruction: Util.formStrToBitArray(`${M_INSTRUCTION_PRFIX}100`),
+        },
+        expected: {
+          pc: JUMP_PC,
+        },
+      },
+      // D&M; JLT -> no jump
+      {
+        inputs: {
+          inM: M,
+          instruction: Util.formStrToBitArray(
+            `${D_AND_M_INSTRUCTION_PRFIX}100`
+          ),
+        },
+        expected: {
+          pc: NO_JUMP_PC,
+        },
+      },
+      // -=-=-=-=-=-=-=-=-=- JNE -=-=-=-=-=-=-=-=-=-=
+      // D; JNE ->  jump
+      {
+        inputs: {
+          inM: M,
+          instruction: Util.formStrToBitArray(`${D_INSTRUCTION_PRFIX}101`),
+        },
+        expected: {
+          pc: JUMP_PC,
+        },
+      },
+      // M; JNE ->  jump
+      {
+        inputs: {
+          inM: M,
+          instruction: Util.formStrToBitArray(`${M_INSTRUCTION_PRFIX}101`),
+        },
+        expected: {
+          pc: JUMP_PC,
+        },
+      },
+      // D&M; JNE -> no jump
+      {
+        inputs: {
+          inM: M,
+          instruction: Util.formStrToBitArray(
+            `${D_AND_M_INSTRUCTION_PRFIX}101`
+          ),
+        },
+        expected: {
+          pc: NO_JUMP_PC,
+        },
+      },
+      // -=-=-=-=-=-=-=-=-=- JLE -=-=-=-=-=-=-=-=-=-=
+      // D; JLE -> no jump
+      {
+        inputs: {
+          inM: M,
+          instruction: Util.formStrToBitArray(`${D_INSTRUCTION_PRFIX}110`),
+        },
+        expected: {
+          pc: NO_JUMP_PC,
+        },
+      },
+      // M; JLE -> jump
+      {
+        inputs: {
+          inM: M,
+          instruction: Util.formStrToBitArray(`${M_INSTRUCTION_PRFIX}110`),
+        },
+        expected: {
+          pc: JUMP_PC,
+        },
+      },
+      // D&M; JLE -> jump
+      {
+        inputs: {
+          inM: M,
+          instruction: Util.formStrToBitArray(
+            `${D_AND_M_INSTRUCTION_PRFIX}110`
+          ),
+        },
+        expected: {
+          pc: JUMP_PC,
+        },
+      },
+      // -=-=-=-=-=-=-=-=-=- JMP -=-=-=-=-=-=-=-=-=-=
+      // D; JMP ->  jump
+      {
+        inputs: {
+          inM: M,
+          instruction: Util.formStrToBitArray(`${D_INSTRUCTION_PRFIX}111`),
+        },
+        expected: {
+          pc: JUMP_PC,
+        },
+      },
+      // M; JMP -> jump
+      {
+        inputs: {
+          inM: M,
+          instruction: Util.formStrToBitArray(`${M_INSTRUCTION_PRFIX}111`),
+        },
+        expected: {
+          pc: JUMP_PC,
+        },
+      },
+      // D&M; JMP -> jump
+      {
+        inputs: {
+          inM: M,
+          instruction: Util.formStrToBitArray(
+            `${D_AND_M_INSTRUCTION_PRFIX}111`
+          ),
+        },
+        expected: {
+          pc: JUMP_PC,
+        },
+      },
+    ];
+
+    for (const experement of experements) {
+      await cpu.eval([Util.random16Bits(), RESET_PC, 1, Util.clock]);
+      await cpu.eval([Util.random16Bits(), JUMP_PC, 0, Util.clock]);
+      await Util.test({ cpu, experement });
+    }
+  });
 });
