342cca9181cc16381abda633c39bb096e4640b52
faisal alhajeri <f.alhajeri@nearpay.io>
2024-06-22 07:45:36 +0300

    parser update

--- code-generation/VM/util/parser/index.ts
+++ code-generation/VM/util/parser/index.ts
@@ -46,6 +46,16 @@ export class VMParser {
       this.current_instruction?.startsWith(VM_ARETHMATIC.gte)
     ) {
       this.current_instruction_type = VM_INTRUCTION_TYPE.C_ARETHMATIC;
+    } else if (this.current_instruction.startsWith("function")) {
+      this.current_instruction_type = VM_INTRUCTION_TYPE.C_FUNCTION;
+    } else if (this.current_instruction.startsWith("label")) {
+      this.current_instruction_type = VM_INTRUCTION_TYPE.C_LABEL;
+    } else if (this.current_instruction.startsWith("call")) {
+      this.current_instruction_type = VM_INTRUCTION_TYPE.C_CALL;
+    } else if (this.current_instruction.startsWith("if-goto")) {
+      this.current_instruction_type = VM_INTRUCTION_TYPE.C_IF;
+    } else if (this.current_instruction.startsWith("return")) {
+      this.current_instruction_type = VM_INTRUCTION_TYPE.C_RETURN;
     } else {
       throw new Error(
         `the instruction ${this.current_instruction} not implemented`
@@ -61,7 +71,12 @@ export class VMParser {
     const splitted: string[] = (trimmed as string)?.split(" ");
     if (
       this.current_instruction_type === VM_INTRUCTION_TYPE.C_PUSH ||
-      this.current_instruction_type === VM_INTRUCTION_TYPE.C_POP
+      this.current_instruction_type === VM_INTRUCTION_TYPE.C_POP ||
+      this.current_instruction_type === VM_INTRUCTION_TYPE.C_CALL ||
+      this.current_instruction_type === VM_INTRUCTION_TYPE.C_FUNCTION ||
+      this.current_instruction_type === VM_INTRUCTION_TYPE.C_GOTO ||
+      this.current_instruction_type === VM_INTRUCTION_TYPE.C_IF ||
+      this.current_instruction_type === VM_INTRUCTION_TYPE.C_LABEL
     ) {
       return splitted[1];
     } else if (
@@ -80,13 +95,21 @@ export class VMParser {
     const splitted: string[] = (trimmed as string)?.split(" ");
     if (
       this.current_instruction_type === VM_INTRUCTION_TYPE.C_PUSH ||
-      this.current_instruction_type === VM_INTRUCTION_TYPE.C_POP
+      this.current_instruction_type === VM_INTRUCTION_TYPE.C_POP ||
+      this.current_instruction_type === VM_INTRUCTION_TYPE.C_CALL ||
+      this.current_instruction_type === VM_INTRUCTION_TYPE.C_FUNCTION
     ) {
       return splitted[2];
     } else if (
-      this.current_instruction_type === VM_INTRUCTION_TYPE.C_ARETHMATIC
+      this.current_instruction_type === VM_INTRUCTION_TYPE.C_ARETHMATIC ||
+      this.current_instruction_type === VM_INTRUCTION_TYPE.C_GOTO ||
+      this.current_instruction_type === VM_INTRUCTION_TYPE.C_IF ||
+      this.current_instruction_type === VM_INTRUCTION_TYPE.C_LABEL ||
+      this.current_instruction_type === VM_INTRUCTION_TYPE.C_RETURN
     ) {
-      throw new Error(`can't use arg2 with C_ARETHMATIC instruction type`);
+      throw new Error(
+        `can't use arg2 with ${this.current_instruction_type} instruction type`
+      );
     } else {
       throw new Error(
         `unimplemented arg2 for ${this.current_instruction_type}`
